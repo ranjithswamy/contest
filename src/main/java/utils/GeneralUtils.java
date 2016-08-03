e64e19ac6936934699f9fa219e0d62924ab279ee
ptamang <parikchit.tamang@amadeus.com>
2016-08-03 09:26:02 +0530

    parikchit updated for GeneralUtils.java

--- src/main/java/utils/GeneralUtils.java
+++ src/main/java/utils/GeneralUtils.java
@@ -1,9 +1,14 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Properties;
 
@@ -12,6 +17,7 @@ public class GeneralUtils {
 	static Properties prop = new Properties();
 	static OutputStream output = null;
 	static InputStream input = null;
+	static String strOutputFilePath = "properties/config.properties";
 	
 	
 	/**
@@ -22,7 +28,7 @@ public class GeneralUtils {
 		
 		try{
 			
-			output = new FileOutputStream("properties/config.properties");
+			output = new FileOutputStream(strOutputFilePath);
 			// set the properties value
 		    prop.setProperty("product", "MerCI");
 		    prop.setProperty("airline", "SQ");
@@ -36,10 +42,13 @@ public class GeneralUtils {
 		}		
 	}
 	
-	
+	/**
+	 * reads a property file * 
+	 * 
+	 */
 	public static void readProperyFile(){
 	try{	
-		input = new FileInputStream("properties/config.properties");
+		input = new FileInputStream(strOutputFilePath);
 
 		// load a properties file
 		prop.load(input);
@@ -52,5 +61,79 @@ public class GeneralUtils {
 		}catch(IOException e){
 			e.printStackTrace();
 		}
+	}
+	
+
+	/**
+	 * 
+	 * deletes older file if already exists.
+	 * 
+	 */
+	public static void deleteFileIfExists(){
+		
+		File f = new File(strOutputFilePath);
+		 if(f.exists()){
+			 f.delete();
+			 System.out.println("Old file found and deleted.");
+		 }
+	}
+	
+	
+	/**
+	 * 
+	 * Write into property file ,as key and value.
+	 * @param strKey
+	 * @param strValue
+	 * @throws IOException
+	 */
+	
+	  public static void addValue(String strKey, String strValue) throws IOException {
+
+		    FileWriter fstream = new FileWriter(strOutputFilePath, true);
+		    BufferedWriter out = new BufferedWriter(fstream);
+		    out.write(strKey + " = " + strValue);
+		    out.newLine();
+		    out.close();
+		    // System.out.println("Added value for " + strKey + " : " + strValue);
+		  }
+	
+	/**
+	 * Set get Value as defined by Key
+	 * 
+	 * 
+	 * @param strKey
+	 * @return
+	 */
+	  public static String getValue(String strKey){
+		    
+		  try{
+		  File file = new File(strOutputFilePath);
+		    BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		    String iLine;
+		    while ((iLine = br.readLine()) != null) {
+		      if (iLine.substring(0, iLine.indexOf('=')).trim().equalsIgnoreCase(strKey.trim())) {
+		        break;
+		      }
+		    }
+		    if (iLine != null) {
+		      // String[] strOutputValue = iLine.split("\\=");
+		      String strOutputValue = "";
+		      if (iLine.contains("=")) {
+		        strOutputValue = iLine.substring(iLine.indexOf('=') + 1);
+		      }
+		      br.close();
+		      // return strOutputValue[1].trim();
+		      return strOutputValue.trim();
+		    }
+		    else {
+		      br.close();
+		      return "";
+		    }
+		  }catch(Exception e){
+			  
+			  e.printStackTrace();
+			  System.out.println("");
+			  return "";
+		  }
 	}
 }
--- src/main/java/utils/sampleCalls.java
+++ src/main/java/utils/sampleCalls.java
@@ -1,12 +1,24 @@
 package utils;
 
+import java.io.IOException;
+
 public class sampleCalls {
 
 	
-	public static void main(String args[]) throws InterruptedException{
+	public static void main(String args[]) throws InterruptedException, IOException{
 		
-		GeneralUtils.writePropertyFile();
+//		GeneralUtils.writePropertyFile();
+//		Thread.sleep(2000);
+//		GeneralUtils.readProperyFile();
+		
+		GeneralUtils.deleteFileIfExists();
+		GeneralUtils.addValue("Company", "Amadeus");
+		GeneralUtils.addValue("Department", "MCC");
+		GeneralUtils.addValue("Team", "MerCI");
 		Thread.sleep(2000);
-		GeneralUtils.readProperyFile();
+		
+		System.out.println(GeneralUtils.getValue("Company"));
+		System.out.println(GeneralUtils.getValue("Department"));
+		System.out.println(GeneralUtils.getValue("Team"));
 	}
 }
